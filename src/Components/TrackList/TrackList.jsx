2c8be3c1235dc3fa3319a1b4f4bb04b2ac8fd2c3
ChalkyVaati <vincecalupad@gmail.com>
2024-09-26 14:55:17 -0400

    CSS Changes

--- src/Components/TrackList/TrackList.jsx
+++ src/Components/TrackList/TrackList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Tracklist.module.css";
+import styles from "./TrackList.module.css";
 import Track from "../Track/Track";
 function Tracklist(props) {
   return (
